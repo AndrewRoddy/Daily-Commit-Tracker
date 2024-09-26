4e891c683517eea203255d06005e84f14c8d1836
Andrew Roddy <andrewroddy7@gmail.com>
2024-09-26 07:57:56 -0400

    0.00.30 curl is NOT working

--- Tracker/main.cpp
+++ Tracker/main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
-#include <iostream>
-#include <string>
-
-// Downloaded for you :)
-#include "..\curl\curl-8.10.1_1-win64-mingw\curl-8.10.1_1-win64-mingw\include\curl\curl.h"
-#include "..\nlohmann\json.hpp"
-
-using std::cout; using std::cin; using std::endl;
+//#include "..\curl\curl-8.10.1_1-win64-mingw\curl-8.10.1_1-win64-mingw\include\curl\curl.h"
+#include <curl/curl.h>
 
 int main() {
-    cout << "Started";
+    std::cout << "Starting CURL test..." << std::endl;
+
+    CURL* curl = curl_easy_init();
+    if (curl) {
+        std::cout << "CURL initialized successfully." << std::endl;
+        curl_easy_cleanup(curl);
+    } else {
+        std::cout << "Failed to initialize CURL" << std::endl;
+    }
+
+    system("PAUSE");
+
     return 0;
 }
