4d0cb25996e3a355d77b95ad25c82b3eb448b7b4
Andrew Roddy <andrewroddy7@gmail.com>
2024-10-03 17:57:46 -0400

    0.01.13 Uses ctime to convert to correct time zone

--- Tracker/main.cpp
+++ Tracker/main.cpp
@@ -7,15 +7,12 @@
 #include <filesystem> // Just to check for TOKEN.env
 #include <ctime> // Gets today's time
 #include <sstream> // For concatenating times
-#include <chrono>
+#include <unordered_map>
 
 using std::cout; using std::cin; using std::endl; using std::string;
 
 static size_t WriteCallback(void*,size_t,size_t,string*);
-
-
-// Gets a json file
-void getJson(string filename, string url, string token, string& readBuffer); 
+void getJson(string filename, string url, string token, string& readBuffer); // Gets a json file
 string getTokenPath(); // Gets the correct token path
 string getToken(); // Gets the token
 string getUtcToday(); // Gets the current date in UTC
@@ -23,27 +20,105 @@ bool checkRepoCommit(nlohmann::json jsonData, string today);
 bool checkAllCommit(string today, string token);
 std::vector<string> getRepos(string filename, string &readBuffer, string token);
 
+std::tm parseUTCTime(const std::string& utcTime) {
+    std::tm timeStruct = {};
+    std::istringstream ss(utcTime);
+    ss >> std::get_time(&timeStruct, "%Y-%m-%dT%H:%M:%SZ");
+
+    // Set the tm_isdst to 0 to avoid daylight saving time adjustments
+    timeStruct.tm_isdst = 0;
+    return timeStruct;
+}
+
+std::time_t timegm(struct std::tm *tm) {
+    std::time_t ret = std::mktime(tm);
+    return ret - _timezone; // Adjust for the local time zone difference
+}
+
+std::string convertToTimeZone(const std::string& utcTime, int timeZoneOffset) {
+    // Parse the input UTC time string
+    std::tm utcTm = parseUTCTime(utcTime);
+
+    // Convert to time_t (epoch time) treating it as UTC
+    std::time_t utcTimeT = timegm(&utcTm);  // timegm() treats time as UTC
+
+    // Adjust the time for the given time zone offset (hours to seconds)
+    utcTimeT += (timeZoneOffset * 3600);
+
+    // Convert back to std::tm (this gives time in UTC again)
+    std::tm* localTm = std::gmtime(&utcTimeT);
+
+    // Format the adjusted time as a string
+    std::ostringstream oss;
+    oss << std::put_time(localTm, "%Y-%m-%d %H:%M:%S");
+    return oss.str();
+}
+
+int getTimeZoneOffset(const std::string& timeZone) {
+    // Map of common time zones to their offsets from UTC
+    std::unordered_map<std::string, int> timeZoneOffsets = {
+        {"UTC", 0},
+        {"EST", -5},
+        {"EDT", -4}, // Eastern Daylight Time
+        {"CST", -6},
+        {"CDT", -5}, // Central Daylight Time
+        {"MST", -7},
+        {"MDT", -6}, // Mountain Daylight Time
+        {"PST", -8},
+        {"PDT", -7}  // Pacific Daylight Time
+    };
+
+    if (timeZoneOffsets.find(timeZone) != timeZoneOffsets.end()) {
+        return timeZoneOffsets[timeZone];
+    } else {
+        std::cout << "Unknown time zone: " << timeZone << std::endl;
+        return 0; // Default to UTC if unknown
+    }
+}
+
 int main() {
     string today = getUtcToday();
     string token = getToken();
-
     /*
-    string readBuffer;
-    string commits_url = "https://api.github.com/repos/AndrewRoddy/Daily-Commit-Tracker/commits"; // Creates proper repo url
-    getJson("Daily-Commit-Tracker", commits_url, token, readBuffer);
-    nlohmann::json jsonData = nlohmann::json::parse(readBuffer);
-    cout << endl << "Getting commits from " << "Daily-Commit-Tracker" << '.';
+    int timeZoneOffset;
+    string timeZone, tzTime;
+    std::string utcTime = "2024-10-01T12:11:08Z";
+
+    timeZone = "UTC";
+    timeZoneOffset = getTimeZoneOffset(timeZone);
+    cout << timeZoneOffset << endl;
+    tzTime = convertToTimeZone(utcTime, timeZoneOffset);
+    std::cout << "Converted time: " << tzTime << std::endl;
+
+    timeZone = "EST";
+    timeZoneOffset = getTimeZoneOffset(timeZone);
+    tzTime = convertToTimeZone(utcTime, timeZoneOffset);
+    std::cout << "Converted time: " << tzTime << std::endl;
+
+    timeZone = "EDT";
+    
+    std::cout << "Converted time: " << tzTime << std::endl;
+    //string readBuffer;
+    //string commits_url = "https://api.github.com/repos/AndrewRoddy/Daily-Commit-Tracker/commits"; // Creates proper repo url
+    //getJson("Daily-Commit-Tracker", commits_url, token, readBuffer);
+    //nlohmann::json jsonData = nlohmann::json::parse(readBuffer);
+    //cout << endl << "Getting commits from " << "Daily-Commit-Tracker" << '.';
     string long_date, date;
+    //cout << endl;
+    long_date = "2024-10-01T18:27:59Z";
+    cout << long_date << '.' << endl;
+    cout << long_date << endl;
+
+
     for (const auto& event : jsonData) {
-        //try{
-        
         long_date = (event["commit"]["committer"]["date"]);
-        ///////
+        
+        ////////
         //const std::chrono::zoned_time cur_time{ std::chrono::current_zone(), std::chrono::system_clock::now() };
-            std::cout << cur_time << '\n';
+ 
 
         ///////
-        cout << long_date << endl;
+        
             //date = long_date.substr(0, 10);
             //cout << long_date << " | " << endl;
         //} catch (...){cout << " ";}
@@ -51,7 +126,7 @@ int main() {
     }
     //cout << "false";
     */
-    ///*
+
     cout << today;
     bool commit = checkAllCommit(today, token);
     if (commit){
@@ -59,7 +134,7 @@ int main() {
     } else {
         cout << endl << "You did not commit today :(";
     }
-    //*/
+    
     
 }
 
@@ -96,12 +171,13 @@ std::vector<string> getRepos(string filename, string &readBuffer, string token){
 
 // Checks repository for commit
 bool checkRepoCommit(nlohmann::json jsonData, string today){
-    string long_date, date;
+    string long_date, convert_date, date;
     for (const auto& event : jsonData) {
         try{
             long_date = (event["commit"]["committer"]["date"]);
-            date = long_date.substr(0, 10);
-            cout << long_date << " | " << endl;
+            convert_date = convertToTimeZone(long_date, getTimeZoneOffset("EDT"));
+            date = convert_date.substr(0, 10);
+            cout << date << " | " << endl;
         } catch (...){cout << " ";}
         if (date == today){return true;}
     }
