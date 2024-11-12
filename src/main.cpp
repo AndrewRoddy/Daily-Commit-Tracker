bba04ddf544009088f4548a4db0de38f45843db8
Andrew Roddy <andrewroddy7@gmail.com>
2024-11-12 08:57:59 -0500

    0.10.17 Fixes GetMessage to GetPeek

--- src/main.cpp
+++ src/main.cpp
@@ -20,47 +20,49 @@ using std::cout; using std::cin; using std::endl; using std::string;
 
 int main() {
 
-    token_check();
-    
-    string today; bool commit;
-    string token = getToken();
+    token_check(); // Checks and creates tokens
+    string token = getToken(); // Gets the current token
 
+    // Tray Icon Functions
     WNDCLASS wc = trayStart();
     HWND hwnd = createWindow();
     HICON hIcon = createIcon("..\\assets\\icons\\32x\\Loading.ico");
     NOTIFYICONDATA nid = createData(hwnd, hIcon);
 
-    int second_loop = 60; // The amount of seconds between .json checks
+    // *Start at high number to check for commits before waiting
+    int second_loop = 60; // Second loop index 
+    int check_interval = 30; // The amount of seconds between .json checks
+    string today; bool commit; MSG msg; // Message Loop
 
-    MSG msg; // Message Loop
     while (true){
         // Checks for if the user presses the icon.
-        if(GetMessage(&msg, NULL, 0, 0)){
+
+        /* 
+        GetMessage() pauses the program if queue is empty
+        PeekMessage() gets and handles user input
+        PM_REMOVE removes the message from queue after peeking */
+        if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)){ 
             TranslateMessage(&msg);
             DispatchMessage(&msg);
-        } else {
-            exit(0);
         }
 
         // Checks for new commits every 30 seconds.
         second_loop += 1;
-        if (second_loop >= 30){
-            today = getToday(); cout << today;
-            commit = checkAllCommit(today, token);
-            removeIcon(nid, hIcon);
-            if (commit){ cout << " Yes" << endl;
+        if (second_loop >= 30){ // Checks
+            today = getToday(); // Gets current day
+            cout << today; // Prints current date to console
+            commit = checkAllCommit(today, token); // True if commit today
+            removeIcon(nid, hIcon); // Clears old icon to show new
+            if (commit){ cout << " Yes" << endl; // Yes Commit -> Green Box
                 hIcon = createIcon("..\\assets\\icons\\32x\\Green.ico");
-            } else { cout << " No" << endl;
+            } else { cout << " No" << endl; // No Commit -> Empty Box
                 hIcon = createIcon("..\\assets\\icons\\32x\\Empty.ico");
             }
-
-            nid = createData(hwnd, hIcon);
-            second_loop = 0;
+            nid = createData(hwnd, hIcon); // Loads the next icon
+            second_loop = 0; // Resets second loop
         }
-    
-        
 
-        // Sleep for 1 minute // Apparently this is less prone to errors?
+        // Sleep for 1 second
         std::this_thread::sleep_for(std::chrono::seconds(1)); 
         
         
